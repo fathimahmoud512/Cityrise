dfd1d0df5cbef9f1ac11347d47f232d165f214ed
fathimahmoud512 <154588017+fathimahmoud512@users.noreply.github.com>
2024-12-18 15:39:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ document.getElementById('signup-form').addEventListener('submit', function(event
     event.preventDefault();  // منع إعادة تحميل الصفحة
 
     // الحصول على القيم المدخلة من قبل المستخدم
-    const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     // يمكنك إضافة الكود هنا لإرسال البيانات إلى الخادم أو تخزينها في LocalStorage
 
-    console.log('اسم المستخدم:', name);
     console.log('البريد الإلكتروني:', email);
     console.log('كلمة المرور:', password);
 
